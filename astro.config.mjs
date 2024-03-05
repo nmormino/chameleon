91ecd95afc8ea789d3c1244c906e44a1dff992e7
Noah Mormino <nmormino82@gmail.com>
2024-03-04 22:26:11 -0600

    Add vercel plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,12 @@ import { defineConfig } from 'astro/config';
 
 import react from "@astrojs/react";
 
+// Import /serverless for a Serverless SSR site
+import vercelServerless from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  output: 'server',
+  adapter: vercelServerless(),
 });
