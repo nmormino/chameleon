b2db11e95f864fa8c2c7568a8fdec706a167998d
Noah Mormino <nmormino82@gmail.com>
2024-03-05 21:32:00 -0600

    Convert to static from server

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from 'astro/config';
 
 import react from "@astrojs/react";
 // Import /serverless for a Serverless SSR site
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  output: 'server',
+  output: 'static',
   adapter: vercel(),
 });
