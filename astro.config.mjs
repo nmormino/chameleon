e6bde3b56962f560d3bdb95f1b760737d466882c
Noah Mormino <nmormino82@gmail.com>
2024-03-05 20:40:32 -0600

    Add /.vercel to .gitignore

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config';
 
 import react from "@astrojs/react";
-
 // Import /serverless for a Serverless SSR site
-import vercelServerless from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   output: 'server',
-  adapter: vercelServerless(),
+  adapter: vercel(),
 });
