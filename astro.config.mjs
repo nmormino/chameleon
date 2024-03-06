ffb8424adaf5ebd5c8e6c88460eee5c64e153845
Noah Mormino <nmormino82@gmail.com>
2024-03-05 22:12:29 -0600

    Github deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 import react from "@astrojs/react";
-// Import /serverless for a Serverless SSR site
-import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  site: 'https://nmormino.github.io',
+  base: '/chameleon',
 });
