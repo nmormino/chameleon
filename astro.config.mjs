cc9bdd16461ef4bf8307da8a5d0c1c3c88752b9b
Noah Mormino <nmormino82@gmail.com>
2024-04-03 20:58:53 -0500

    Update astro config for vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: 'https://nmormino.github.io',
-  base: '/chameleon',
+  output: "server",
+  adapter: vercel()
 });
